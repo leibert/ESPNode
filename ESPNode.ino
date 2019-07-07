f78bfc44ec4b77031a0d6c707171f69cb72664c0
leibert <cwm@eml.cc>
2019-07-06 23:28:42 -0400

    rewrite -fallback to softap if unable to connect to station -send and save config -new frontend

--- ESPNode.ino
+++ ESPNode.ino
@@ -1,32 +1,40 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266WiFiMulti.h>
+#include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
+#include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <FS.h> // Include the SPIFFS library
 #include "espDMX.h"
 
 //GLOBAL CONFIG VARIABLES (filled out by SPIFF read of config.dat)
-char nodename[] = "DevMEMTEST";
+char nodename[64] = "ESPNode";
 // Node Description
-char nodedesc[] = "Development Memory Test";
+char nodedesc[64] = "Unconfigured ESPNode";
 
 // WIFI Credentials. Set default as values in passwords.h
-const char *WIFIssid;
-const char *WIFIpassword;
+char WIFIssid[64];
+char WIFIpassword[64];
 
 //remote location of IOS Files
 char IOSResources[] = "ioshit.net";
 
-ESP8266WiFiMulti wifiMulti; // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
+bool validWIFI = false;
 
+//setup for softAP if needed
+const byte DNS_PORT = 53;
+IPAddress apIP(192, 168, 1, 1);
+
+DNSServer dnsServer;
 ESP8266WebServer server(80); // Create a webserver object that listens for HTTP request on port 80
 
 String getContentType(String filename); // convert the file extension to the MIME type
 bool handleFileRead(String path);		// send the right file to the client (if it exists)
 
+String buffer = "";
+
 //read config file
-void processconfigfile(File f)
+void loadConfigJSONSettings(File f)
 {
 	char line[128];
 	char *key, *value;
@@ -35,40 +43,130 @@ void processconfigfile(File f)
 	// read file line by line
 	while (f.available())
 	{
-		//strcpy(line, "");
-		// Serial.println("***************");
-		// Serial.println(line);
-		charcount = f.readBytesUntil('\n', line, 128);
+		charcount = f.readBytesUntil(',', line, 128);
 		// Serial.println(line);
 		// Serial.println(charcount);
-		line[charcount + 1] = '\0';
-		// Serial.println(line);
-
-		//Serial.println(strncmp("WIFI-SSID", line, strlen("WIFI-SSID")));
-		if (strncmp("WIFI-SSID", line, strlen("WIFI-SSID")) == 0)
+		line[charcount] = '\0';
+		Serial.println("!!!!!!!!!!!!!!!!!");
+		Serial.println(line);
+		Serial.println("!!!!!!!!!!!!!!!!!");
+		//make sure we're in the ESPconfig JSON object
+		// key = strtok(line, "\"ESPCONFIG\"");
+		Serial.print(line[0]);
+		if (line[0] == '"' && line != NULL)
 		{
-			Serial.println("SSID GRAB");
+			memmove(line + 1, line, strlen(line) + 1);
+			line[0] = ' ';
+			Serial.println("line shifted");
 			Serial.println(line);
-			key = strtok(line, "=");
-			value = strtok(NULL, "\n");
-
-			Serial.println("KV SSID");
-			Serial.println(key);
-			Serial.println(value);
-			memcpy(WIFIssid, value, sizeof(value));
 		}
-		else if (strncmp("WIFI-PW", line, strlen("WIFI-PW")) == 0)
+
+		key = strtok(line, "\"");
+
+		while (key != NULL && line[0] != '\0')
 		{
-			Serial.println("password GRAB");
+			Serial.println("***************");
 			Serial.println(line);
-			key = strtok(line, "=");
-			value = strtok(NULL, "\n");
+			key = strtok(NULL, "\"");
+			Serial.println("KEY:");
+			if (!key)
+			{
+				Serial.println("loop break");
+				break;
+			}
+			// Serial.println(key);
 
-			Serial.println("KV password");
-			Serial.println(key);
-			Serial.println(value);
-			memcpy(WIFIpassword, value, sizeof(value));
+			//check to see if this is a key we want a value for
+			if (strcmp(key, "ESPconfig") == 0)
+			{
+				Serial.println("header--ignore");
+			}
+			else if (strcmp(key, "WIFISSID") == 0)
+			{
+				Serial.println("wifissidset");
+				value = strtok(NULL, "\"");
+				value = strtok(NULL, "\"");
+				strcpy(WIFIssid, value);
+			}
+			else if (strcmp(key, "WIFIPW") == 0)
+			{
+				Serial.println("wifipwset");
+
+				value = strtok(NULL, "\"");
+				value = strtok(NULL, "\"");
+				Serial.print("wifi pw");
+				Serial.print(value);
+				strcpy(WIFIpassword, value);
+			}
+			else if (strcmp(key, "NODENAME") == 0)
+			{
+				Serial.println("nodenameset");
+
+				value = strtok(NULL, "\"");
+				value = strtok(NULL, "\"");
+				strcpy(nodename, value);
+			}
+			else if (strcmp(key, "NODEDESC") == 0)
+			{
+				Serial.println("nodedescset");
+
+				value = strtok(NULL, "\"");
+				value = strtok(NULL, "\"");
+				strcpy(nodedesc, value);
+			}
+			else if (strcmp(key, "IOSRESOURCES") == 0)
+			{
+				Serial.println("IOSRESOURCESset");
+
+				value = strtok(NULL, "\"");
+				value = strtok(NULL, "\"");
+				strcpy(IOSResources, value);
+			}
+			else
+			{
+				Serial.println("trash this value...unknown key");
+				value = strtok(NULL, "\"");
+				value = strtok(NULL, "\"");
+			}
+
+			//done processing this key...advance to next
+			// Serial.println("line now");
+			// Serial.print(line);
+			// if (line[0])
+			// {
+			key = strtok(NULL, "\"");
+			// }
 		}
+
+		// //Serial.println(strncmp("WIFI-SSID", line, strlen("WIFI-SSID")));
+		// if (strstr(line, "WIFISSID"))
+		// {
+		// 	Serial.println("SSID GRAB");
+		// 	Serial.println(line);
+		// 	key = strtok(line, "\"");
+		// 	key = strtok(NULL, "\"");
+		// 	value = strtok(NULL, "\"");
+		// 	value = strtok(NULL, "\"");
+
+		// 	Serial.println("KV SSID");
+		// 	Serial.println(key);
+		// 	Serial.println(value);
+		// 	strcpy(WIFIssid, value);
+		// }
+		// else if (strstr(line, "WIFIPW"))
+		// {
+		// 	Serial.println("password GRAB");
+		// 	Serial.println(line);
+		// 	key = strtok(line, "\"");
+		// 	key = strtok(NULL, "\"");
+		// 	value = strtok(NULL, "\"");
+		// 	value = strtok(NULL, "\"");
+
+		// 	Serial.println("KV password");
+		// 	Serial.println(key);
+		// 	Serial.println(value);
+		// 	strcpy(WIFIpassword, value);
+		// }
 
 		// char *key = strtok(line, '=')
 
@@ -110,6 +208,41 @@ void processconfigfile(File f)
 	}
 }
 
+void setupWebServer(bool configMode)
+{
+	if (MDNS.begin("esp8266"))
+	{ // Start the mDNS responder for esp8266.local
+		Serial.println("mDNS responder started");
+	}
+	else
+	{
+		Serial.println("Error setting up MDNS responder!");
+	}
+
+	if (configMode)
+	{
+		Serial.println("switch to config index");
+		server.on("/", HTTP_GET, [&]() {
+			const char *metaRefreshStr = "<head><meta http-equiv=\"refresh\" content=\"0; url=http://192.168.1.1/configpanel.html\" /></head><body><p>redirecting...</p></body>";
+			server.send(200, "text/html", metaRefreshStr);
+		});
+	}
+
+	server.on("/CFG", HTTP_ANY, handleWEBConfig); // Call the 'handleLED'
+
+	server.onNotFound([&]() { // If the client requests any URI
+		if (!handleFileRead(server.uri()))
+		{ // send it if it exists
+			Serial.println("captive catch");
+			const char *metaRefreshStr = "<head><meta http-equiv=\"refresh\" content=\"0; url=/\" /></head><body><p>redirecting...</p></body>";
+			server.send(200, "text/html", metaRefreshStr);
+		}
+	});
+
+	server.begin(); // Actually start the server
+	Serial.println("HTTP server started");
+}
+
 void setup()
 {
 	Serial.begin(115200); // Start the Serial communication to send messages to the computer
@@ -128,50 +261,60 @@ void setup()
 	{
 		Serial.println("====== Reading from SPIFFS file =======");
 		//process config file
-		processconfigfile(f);
+		loadConfigJSONSettings(f);
 	}
 	Serial.println("trying to setup wifi");
 	Serial.println("SSID:");
 	Serial.println(WIFIssid);
 	Serial.println("PW:");
 	Serial.println(WIFIpassword);
-
-	wifiMulti.addAP(WIFIssid, WIFIpassword); // add Wi-Fi networks you want to connect to
-	// wifiMulti.addAP("walrus", "woodchuck");  // add Wi-Fi networks you want to connect to
+	WiFi.persistent(false);
+	// WiFi.begin(WIFIssid, WIFIpassword); // add Wi-Fi networks you want to connect to
+	WiFi.begin("badap", "badpw"); // add Wi-Fi networks you want to connect to
 
 	Serial.println("Connecting ...");
 	int i = 0;
-	while (wifiMulti.run() != WL_CONNECTED)
+	while (WiFi.status() != WL_CONNECTED)
 	{ // Wait for the Wi-Fi to connect
+		//assume there will be a valid wifi connection
+		validWIFI = true;
 		delay(250);
 		Serial.print('.');
+		i++;
+		if (i > 10)
+		{
+			Serial.println("WIFI FAIL");
+			validWIFI = false;
+			break;
+		}
 	}
-	Serial.println('\n');
-	Serial.print("Connected to ");
-	Serial.println(WiFi.SSID()); // Tell us what network we're connected to
-	Serial.print("IP address:\t");
-	Serial.println(WiFi.localIP()); // Send the IP address of the ESP8266 to the computer
 
-	if (MDNS.begin("esp8266"))
-	{ // Start the mDNS responder for esp8266.local
-		Serial.println("mDNS responder started");
+	if (validWIFI)
+	{
+		Serial.println('\n');
+		Serial.print("Connected to ");
+		Serial.println(WiFi.SSID()); // Tell us what network we're connected to
+		Serial.print("IP address:\t");
+		Serial.println(WiFi.localIP()); // Send the IP address of the ESP8266 to the computer
+		setupWebServer(false);
 	}
 	else
 	{
-		Serial.println("Error setting up MDNS responder!");
+		WiFi.disconnect();
+		WiFi.mode(WIFI_AP);
+		WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+		WiFi.softAP("ESPCONFIG");
+		dnsServer.start(DNS_PORT, "*", apIP);
+		setupWebServer(true);
 	}
-
-	server.onNotFound([]() {								  // If the client requests any URI
-		if (!handleFileRead(server.uri()))					  // send it if it exists
-			server.send(404, "text/plain", "404: Not Found"); // otherwise, respond with a 404 (Not Found) error
-	});
-
-	server.begin(); // Actually start the server
-	Serial.println("HTTP server started");
 }
 
 void loop(void)
 {
+	if (!validWIFI)
+	{
+		dnsServer.processNextRequest();
+	}
 	server.handleClient();
 }
 
@@ -190,6 +333,7 @@ String getContentType(String filename)
 
 bool handleFileRead(String path)
 { // send the right file to the client (if it exists)
+	// path = "html" + path;
 	Serial.println("handleFileRead: " + path);
 	if (path.endsWith("/"))
 		path += "index.html";				   // If a folder is requested, send the index file
@@ -203,4 +347,83 @@ bool handleFileRead(String path)
 	}
 	Serial.println("\tFile Not Found");
 	return false; // If the file doesn't exist, return false
+}
+
+void handleWEBConfig()
+{
+	Serial.println("in handle WEB config");
+	Serial.println("recieved ARGS are");
+	// for (int i = 0; i < server.args(); i++)
+	// {
+
+	// 	Serial.print(server.argName(i));
+	// 	Serial.println(server.arg(i));
+	// }
+
+	if (server.method() == HTTP_POST)
+	{
+		Serial.println("CONFIG POST");
+		buffer = server.argName(0);
+		Serial.println("inputis");
+		Serial.println(buffer);
+		if (buffer.startsWith("{\"ESPconfig\":"))
+		{
+			saveConfigJSON(buffer);
+		}
+	}
+	else if (server.method() == HTTP_GET)
+	{
+		Serial.println("CONFIG GET");
+		Serial.println(server.argName(0));
+		// Serial.println(server.argName(1));
+
+		if (server.args() && server.argName(0) == "getConfig")
+		{
+			Serial.println("createConfigJSON");
+			server.send(200, "text/html", getConfigJSON());
+		}
+		Serial.println();
+	}
+	else
+	{
+		Serial.println("OTHER METHOD");
+	}
+
+	// Serial.println(server.arg);
+}
+
+String getConfigJSON()
+{
+	buffer = "";
+	buffer += "{";
+
+	buffer += "\"ESPconfig\":{";
+	buffer += "\"WIFISSID\":\"" + String(WIFIssid) + "\",";
+	buffer += "\"WIFIPW\":\"" + String(WIFIpassword) + "\",";
+	buffer += "\"WIFISTATUS\":\"" + String(validWIFI) + "\",";
+	buffer += "\"NODENAME\":\"" + String(nodename) + "\",";
+	buffer += "\"NODEDESC\":\"" + String(nodedesc) + "\",";
+	buffer += "\"IOSRESOURCES\":\"" + String(IOSResources) + "\",";
+	buffer += "\"NUMCH\":\"TEST\"";
+	buffer += "}";
+
+	buffer += "}";
+	return buffer;
+}
+
+void saveConfigJSON(String configBuffer)
+{
+	File f = SPIFFS.open("/config.dat", "w+");
+	if (!f)
+	{
+		Serial.println("file open failed");
+	}
+	else
+	{
+		f.print(configBuffer);
+		Serial.println("config written");
+		Serial.println("resetting node");
+		f.close();
+		ESP.reset();
+	}
 }
---
+++ old/data/configpanel.js
@@ -0,0 +1,353 @@
+var config = "NO DATA RECIEVED";
+
+
+
+$('#numCH').on('input', function() {
+  console.log("number of channels changed");
+  changeCHvisbility();
+});
+
+
+function changeCHtype(chID) {
+  var chselector = "#" + chID;
+
+  //Configure Values
+
+  //1 CH Switch
+  if ($(chselector + "type").val() == 1) {
+
+    $(chselector + "valA" + ".IOlabel").html("Output Pin: ");
+    $(chselector + "valA" + ".IOlabel").css("display", "inline");
+    $(chselector + "valA" + ".IOinput").css("display", "inline");
+
+    $(chselector + "valB" + ".IOlabel").css("display", "none");
+    $(chselector + "valB" + ".IOinput").css("display", "none");
+
+    $(chselector + "valC" + ".IOlabel").css("display", "none");
+    $(chselector + "valC" + ".IOinput").css("display", "none");
+
+    $(chselector + "inverted" + ".IOinput").css("display", "inline");
+    $(chselector + "inverted" + ".IOlabel").css("display", "inline");
+
+  }
+  //1 CH PWM
+  else if ($(chselector + "type").val() == 2) {
+
+    $(chselector + "valA" + ".IOlabel").html("Output Pin: ");
+    $(chselector + "valA" + ".IOlabel").css("display", "inline");
+    $(chselector + "valA" + ".IOinput").css("display", "inline");
+
+    $(chselector + "valB" + ".IOlabel").css("display", "none");
+    $(chselector + "valB" + ".IOinput").css("display", "none");
+
+    $(chselector + "valC" + ".IOlabel").css("display", "none");
+    $(chselector + "valC" + ".IOinput").css("display", "none");
+
+    $(chselector + "inverted" + ".IOinput").css("display", "inline");
+    $(chselector + "inverted" + ".IOlabel").css("display", "inline");
+
+  }
+  //RGB PWM
+  else if ($(chselector + "type").val() == 3) {
+
+    $(chselector + "valA" + ".IOlabel").html("Output Pin (Red):&nbsp;&nbsp; ");
+    $(chselector + "valA" + ".IOlabel").css("display", "inline");
+    $(chselector + "valA" + ".IOinput").css("display", "inline");
+
+    $(chselector + "valB" + ".IOlabel").html("Output Pin (Green): ");
+    $(chselector + "valB" + ".IOlabel").css("display", "inline");
+    $(chselector + "valB" + ".IOinput").css("display", "inline");
+
+    $(chselector + "valC" + ".IOlabel").html("Output Pin (Blue): &nbsp;&nbsp;");
+    $(chselector + "valC" + ".IOlabel").css("display", "inline");
+    $(chselector + "valC" + ".IOinput").css("display", "inline");
+
+    $(chselector + "inverted" + ".IOinput").css("display", "inline");
+    $(chselector + "inverted" + ".IOlabel").css("display", "inline");
+}
+    //DMX
+    else if ($(chselector + "type").val() == 21) {
+
+      $(chselector + "inverted" + ".IOinput").css("display", "none");
+      $(chselector + "inverted" + ".IOlabel").css("display", "none");
+
+      $(chselector + "valA" + ".IOlabel").html("Profile: ");
+      $(chselector + "valA" + ".IOlabel").css("display", "inline");
+      $(chselector + "valA" + ".IOinput").css("display", "inline");
+
+      $(chselector + "valB" + ".IOlabel").html("DMX Start Channel: ");
+      $(chselector + "valB" + ".IOlabel").css("display", "inline");
+      $(chselector + "valB" + ".IOinput").css("display", "inline");
+
+      $(chselector + "valC" + ".IOlabel").css("display", "none");
+      $(chselector + "valC" + ".IOinput").css("display", "none");
+
+
+
+  } else {
+
+    $(chselector + "ValA" + ".IOlabel").css("display", "none");
+    $(chselector + "ValA" + ".IOinput").css("display", "none");
+
+    $(chselector + "ValB" + ".IOlabel").css("display", "none");
+    $(chselector + "ValB" + ".IOinput").css("display", "none");
+
+    $(chselector + "ValC" + ".IOlabel").css("display", "none");
+    $(chselector + "ValC" + ".IOinput").css("display", "none");
+
+  }
+}
+
+
+
+$(document).on('change', '.CHtype', function() {
+  console.log("input type changed to to" + $(this).val() + "--" + $(this).find(':selected').text());
+  console.log($(this).parent().parent().prop('id'));
+  var chID = $(this).parent().parent().prop('id');
+  changeCHtype(chID);
+});
+
+
+$(document).on('change', '.CHdescinput', function() {
+  console.log("description changed to to" + $(this).val());
+  var selectorstring = "#" + $(this).parent().parent().prop('id') + "DescLabel";
+  console.log(selectorstring);
+  if ($(this).val() != "") {
+    $(selectorstring).html(": " + $(this).val());
+  } else {
+    $(selectorstring).html("");
+  }
+
+});
+
+
+$("form").submit(function(event) {
+      event.preventDefault();
+      //var configstring ="/updconfig&configsecret="+$("#configsecret").val();
+      var configstring = "CONFIGSTRING##\n";
+      configstring += "WIFI-SSID=" + $("#WIFI-SSID").val() + "\n";
+      configstring += "WIFI-PW=" + $("#WIFI-PW").val() + "\n";
+      configstring += "NODENAME=" + $("#nodename").val() + "\n";
+      configstring += "DESCRIPTION=" + $("#nodedesc").val() + "\n";
+      configstring += "IOSRESOURCES=" + $("#iosresources").val() + "\n";
+      configstring += "CONFIGSECRET=" + $("#configsecret").val() + "\n";
+      configstring += "!!CHANNELS-START!!\n";
+
+      //startassembling channels
+      for (var i = 1; i <= $("#numCH").val(); i++) {
+        var channelstring = i + ", " + $("#CH" + i + "type").val() + ", ";
+        channelstring += $("#CH" + i + "valA" + ".IOinput ").val() + ", " + $("#CH" + i + "valB" + ".IOinput ").val() + ", " + $("#CH" + i + "valC" + ".IOinput ").val();
+
+        if($("#CH" + i + "inverted" + ".IOinput ").is(':checked'))
+          channelstring += ", 1";
+        else
+          channelstring += ", 0";
+
+        channelstring += " #" + $("#CH" + i + "desc" + ".CHdescinput ").val();
+
+        console.log(channelstring);
+        channelstring+="\n";
+
+
+        if ($("#CH" + i + "type").val()!=0)
+          configstring += channelstring;
+      }
+
+      configstring += "##CHANNELS-END##\n";
+
+      //configstring+="CONFIGSTRINGEND!!\n";
+      console.log("configsecret is:" + $("#configsecret").val())
+
+      console.log(configstring);
+
+// configstring = "CONFIGSTRING##\
+// WIFI-SSID=66ADAMS\n\
+// WIFI-PW=3ng1n33rs_mans1on\n\
+// IOSRESOURCES=ioshit.net\n\
+// NODENAME=DEVBRD\n\
+// DESCRIPTION=Development Board\n\
+// CONFIGSECRET=defaulterd\n\
+// !!CHANNELS-START!!\n\
+// 1, 3, 14, 12, 13, 0 #Right RGB Flood\n\
+// 2, 3, 4, 5, 16, 0 #Left RGB Flood\n\
+// 3, 20, 301, 1, 0, 0 #DMXA\n\
+// 4, 20, 301, 16, 0, 0 #DMXB\n\
+// 5, 20, 301, 32, 0, 0 #DMXC\n\
+// ##CHANNELS-END##\n";
+
+
+      $.post("/updconfig", configstring, null, "text");
+
+      parseconfig();
+      changeCHvisbility();
+    });
+
+
+
+
+    function changeCHvisbility() {
+      var panelstring = "";
+      console.log("make " + $('#numCH').val() + " channels visible");
+      $('.chdetails').each(function() {
+        $(this).css('display', 'none')
+      });
+
+      for (i = 1; i <= $('#numCH').val(); i++) {
+        var selectorstring = "#CH" + i + ".chdetails";
+        console.log(selectorstring);
+        $(selectorstring).css('display', 'block');
+      }
+
+    }
+
+
+    function buildCHpanel(maxCH = 128) {
+      console.log("building a " + maxCH + "channel panel");
+      var panelstring = "";
+      for (i = 1; i <= maxCH; i++) {
+        panelstring +=
+          "<div class='chdetails' id='CH" + i + "' style='display:none'>\
+    <h3><a class='expandlnk' href='javascript:;' onclick='expandCH(" + i + ")'>+</a> Channel " + i + "<span id='CH" + i + "DescLabel'></span></h3>\
+    <div class='chconfiguration' style='display: none'>\
+    Description: <input type='text' class='CHdescinput' id='CH" + i + "desc' /><br /> \
+    Type: <select class='CHtype' id='CH" + i + "type'> \
+    <option value='0'>IO Disabled</option>\
+    <option value='1'>Binary Switch</option>\
+    <option value='2'>1 CH PWM</option>\
+    <option value='3'>RGB PWM</option>\
+    <option value='21'>DMX</option>\
+    <select/><br> \
+    <span class = 'IOlabel' id='CH" + i + "inverted' style='display:none'>Inverted Output:</span><input type='checkbox' class='IOinput' id='CH" + i + "inverted' style='display:none'/><br> \
+    <span class = 'IOlabel' id='CH" + i + "valA' style='display:none'>Value A:</span><input type='text' class='IOinput' id='CH" + i + "valA' style='display:none'/><br> \
+    <span class = 'IOlabel' id='CH" + i + "valB' style='display:none'>Value B:</span><input type='text' class='IOinput' id='CH" + i + "valB' style='display:none'/><br> \
+    <span class = 'IOlabel' id='CH" + i + "valC' style='display:none'>Value C:</span><input type='text' class='IOinput' id='CH" + i + "valC' style='display:none'/><br> \
+    </div>\
+    <hr />\
+    </div>\
+    ";
+      }
+
+      $('#chdefinitions').html(panelstring);
+    }
+
+
+    function populatechannels() {
+      console.log("in populatechannels");
+    }
+
+    function expandCH(chnum) {
+      //console.log("expanding a Channel");
+      var selectorstring = "#CH" + chnum + " .chconfiguration";
+      //console.log(selectorstring);
+      $(selectorstring).css('display', 'block');
+      var selectorstring = "#CH" + chnum + " .expandlnk";
+      $(selectorstring).attr('onclick', 'collapseCH(' + chnum + ')');
+      $(selectorstring).html('-');
+      $(selectorstring).attr('class', 'collapselnk');
+    }
+
+    function collapseCH(chnum) {
+      //console.log("expanding a Channel");
+      var selectorstring = "#CH" + chnum + " .chconfiguration";
+      //console.log(selectorstring);
+      $(selectorstring).css('display', 'none');
+      var selectorstring = "#CH" + chnum + " .collapselnk";
+      $(selectorstring).attr('onclick', 'expandCH(' + chnum + ')');
+      $(selectorstring).html('+');
+      $(selectorstring).attr('class', 'expandlnk');
+    }
+
+    function parsechannel(chconfig) {
+      console.log(chconfig);
+      var chdesc = chconfig.substring(chconfig.indexOf("#") + 1);
+      console.log(chdesc);
+      chconfig = chconfig.split(",");
+      console.log(chconfig)
+      var chselector = "#CH" + chconfig[0];
+      $(chselector + "desc").val(chdesc);
+      $(chselector + "type").val(chconfig[1].trim());
+      $(chselector + "valA" + ".IOinput ").val(chconfig[2].trim());
+      $(chselector + "valB" + ".IOinput ").val(chconfig[3].trim());
+      $(chselector + "valC" + ".IOinput ").val(chconfig[4].trim());
+      if(chconfig[5].trim().substring(0,1)==1)
+        $(chselector + "inverted" + ".IOinput ").prop( "checked", true );
+      else
+        $(chselector + "inverted" + ".IOinput ").prop( "checked", false );
+
+
+      changeCHtype("CH" + chconfig[0]);
+
+
+
+    }
+
+    function parsechannelblock(config) {
+      console.log("parsing channels");
+      for (var i = 1; i < config.length; i++) {
+        if (config[i] == "##CHANNELS-END##") {
+          console.log("done parsing channels there are " + (i - 1));
+          $("#numCH").val(i - 1);
+          return config.slice(i);
+        } else {
+          parsechannel(config[i]);
+        }
+      }
+      return null;
+    }
+
+    function parseline(line) {
+      if (line.startsWith("WIFI-SSID=")) {
+        $("#WIFI-SSID").val(line.substring(line.indexOf("=") + 1));
+        console.log("ssid");
+      } else if (line.startsWith("WIFI-PW=")) {
+        $("#WIFI-PW").val(line.substring(line.indexOf("=") + 1));
+      } else if (line.startsWith("NODENAME=")) {
+        $("#nodename").val(line.substring(line.indexOf("=") + 1));
+      } else if (line.startsWith("DESCRIPTION=")) {
+        $("#nodedesc").val(line.substring(line.indexOf("=") + 1));
+      } else if (line.startsWith("IOSRESOURCES=")) {
+        $("#iosresources").val(line.substring(line.indexOf("=") + 1));
+      }
+    }
+
+    function parseconfig() {
+      console.log(config);
+      console.log("parsing config");
+      config=config.toString();
+      config = config.split('\n');
+      console.log(config[2]);
+      for (var i = 0; i < config.length; i++) {
+        if (config[i] == "!!CHANNELS-START!!") {
+          config = parsechannelblock(config.slice(i));
+          i = 0;
+        } else {
+          parseline(config[i]);
+        }
+
+      }
+
+
+
+    }
+
+    function getconfig() {
+      console.log("Getting initial config");
+      $.get("/cfginit", dataType = "text", function(data) {
+
+        config = data.toString();
+        parseconfig();
+        changeCHvisbility();
+      });
+
+    }
+
+    function init() {
+      console.log("init function");
+      getconfig();
+      buildCHpanel();
+
+
+    }
+
+
+    init(); console.log("configpanel.js loaded");
